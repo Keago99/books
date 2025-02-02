3b062f68b433df18c8a12e5e6b9ad94b1ea59bdf
Keago99 <keatabde@gmail.com>
2025-02-02 20:23:11 +0200

    Cleaned Books.js and add button to index.html for actual methods required by TOP.

--- Books.js
+++ Books.js
@@ -18,11 +18,3 @@ function addBookToLibrary(title, author, pages, read){
     let bookToAdd = new book(title, author, pages, read)
     myLibrary.push(bookToAdd);
 }
-
-const theHobbit = new book("The Hobbit", "J.R.R Tolkien", "300", "Not read yet");
-
-console.log(theHobbit.info());
-addBookToLibrary("DuckBook", "Mr.Duck", "300", "Not read yet");
-
-console.log(myLibrary); // Works by adding new
-console.log(myLibrary[0].info()); // Works by adding old
