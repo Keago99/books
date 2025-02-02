095c3d7edc32bc804a8db743d656f8f4d166a320
Keago99 <keatabde@gmail.com>
2025-02-02 20:53:49 +0200

    add basic spacing and dialog to html along with methods for opening and closing modal.

--- Books.js
+++ Books.js
@@ -1,6 +1,9 @@
 
 // Empty arrayList containing all library books
 const myLibrary = [];
+const addBookButton = document.getElementById("addBookButton");
+const bookDialog = document.getElementById("bookDialog");
+const closeModal = document.getElementById("closeModal");
 
 function book(title, author, pages, read){
     this.title = title;
@@ -18,3 +21,12 @@ function addBookToLibrary(title, author, pages, read){
     let bookToAdd = new book(title, author, pages, read)
     myLibrary.push(bookToAdd);
 }
+
+addBookButton.addEventListener("click", () => {
+    bookDialog.showModal();
+});
+
+closeModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    bookDialog.close();
+});
